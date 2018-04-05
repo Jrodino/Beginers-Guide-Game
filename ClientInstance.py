8541b7ab19c047a393119db2550871d2c759aae4
Julio <jrodino14@gmail.com>
2018-04-04 22:54:15 -0300

    ClientInstance works

---
+++ ClientInstance.py
@@ -0,0 +1,15 @@
+from NetClasses import *
+import direct.directbase.DirectStart
+from direct.showbase.DirectObject import DirectObject
+
+class ClientI(DirectObject):
+    def __init__(self):
+        self.setupClient()
+
+    def setupClient(self):
+
+        client = Client(ClientProtocol())
+        client.connect("localhost", 9999, 3000)
+        client.start()
+w = ClientI()
+base.run()
